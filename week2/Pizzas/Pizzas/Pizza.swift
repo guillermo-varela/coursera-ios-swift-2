77f3f2393c9e09e9c6cb455ff589b7850fc67baf
Guillermo Varela <guille.varela3.16@gmail.com>
2016-09-12 06:33:31 +0000

    first commit

---
+++ week2/Pizzas/Pizzas/Pizza.swift
@@ -0,0 +1,13 @@
+//
+//  Pizza.swift
+//  Pizzas
+//
+//  Created by Guillermo Varela on 9/12/16.
+//  Copyright © 2016 Guillermo Varela. All rights reserved.
+//
+
+import Foundation
+
+class Pizza {
+    var size: Size?
+}
